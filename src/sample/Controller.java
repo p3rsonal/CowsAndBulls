96974445e26e3c11a3380c6769f11cd7a9170818
Arthur <artursspirins@gmail.com>
2017-10-17 20:50:28 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TableView;
+
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class Controller {
+    public Spinner<Integer> n1Spinner;
+    public Spinner<Integer> n2Spinner;
+    public Spinner<Integer> n3Spinner;
+    public Spinner<Integer> n4Spinner;
+    public TableView<Turn> resultTable;
+    private int nr = 1;
+    private Set<Integer> rand = new LinkedHashSet<>();
+    // linkedHashSet garantiruet porjadok (kolekcija unikalnih elementov, netu dublikatov)
+
+    public void initialize() {
+        Random r = new Random();
+        while (rand.size() < 4) {
+            int n = r.nextInt(10);
+            rand.add(n);  // proverjaet na unikalnost, tak kak uporjadocheno
+        }
+    }
+    // ne ponjal
+
+    public void doTurn(ActionEvent actionEvent) {
+        Turn t = new Turn();
+        t.setNr(nr);
+        nr++;
+        t.setN1(n1Spinner.getValue());
+        t.setN2(n2Spinner.getValue());
+        t.setN3(n3Spinner.getValue());
+        t.setN4(n4Spinner.getValue());
+
+        resultTable.getItems().add(t);
+    }
+}
